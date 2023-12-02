89cc1256437b722c2138c0aff02b3ab477aa8895
Alex O'Brien <alex8obrien@gmail.com>
2023-12-02 00:12:59 +0000

    Added template for Day02

---
+++ 2023/Day02/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Day02
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            Part1(lines);
+            Part2(lines);
+        }
+
+        private static void Part1(string[] lines)
+        {
+
+        }
+
+        private static void Part2(string[] lines)
+        {
+
+        }
+    }
+}
